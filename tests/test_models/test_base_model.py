c4161aa02f7fe2a8698b739026173776ac3bef21
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-11-11 13:34:48 +0200

    Debug for checker task 3, AirBnB_clone Console

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Implements Unittests for BaseModel class."""
 import unittest
-from models.base_model import BaseModel
+from ...models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
