16a995eaa28a76b978ef51b8b9ab1e301ce3a5d3
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-11-11 13:13:45 +0200

    Debug for checker task 3, AirBnB_clone Console

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
-"""Implements Unittests for BaseModel class."""
+""" This module implements Unit Tests for Base class."""
 import unittest
-from models.base_model import BaseModel
+from ...models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """Defines Unittests for BaseModel class.
+    """ Unit Test class for BaseModel class."""
 
-    Todo:
-        * Test if instance attributes are initialized to correct values.
-        * Test if id is always unique.
-        * Test if save() method updates updated_at attribute correctly.
-        * Test if string representation of instance, returned
-            by __str__() is correct.
-        * Test if dictionary representation of instance, returned by
-            to_dict() is correct.
-    """
-
-    def setUp(self):
-        self.base_model_1 = BaseModel()
-
-    def test_save(self):
-        pass
-
-    def test_to_dict(self):
-        pass
-
-    def test__str__(self):
+    def test_BaseModel_init(self):
         pass
 
 
