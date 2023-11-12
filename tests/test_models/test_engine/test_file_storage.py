7ffa81f21ff35b69982ba78af0700b311c3f3d4b
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-11-12 02:25:00 +0200

    Debug task 5 for checker, AirBnB_clone Console

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module implements Unit Tests for FileStorage class."""
+import unittest
+from ....models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """ Unit Test class for FileStorage class."""
+
+    def test_FileStorage_init(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
