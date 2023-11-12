cf869b34facbba53d35f97b4f6c0ff1d4bbc6ee8
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-11-12 03:07:26 +0200

    Debug task 5 for checker, AirBnB_clone Console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,14 +31,32 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file."""
-        with open(self.__file_path, mode='w') as store_file:
-            json.dump(self.__objects, store_file)
+        # with open(self.__file_path, mode='w') as store_file:
+        #     json.dump(self.__objects, store_file)
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict().copy()
+        with open(FileStorage.__file_path, mode='w') as my_file:
+            json.dump(new_dict, my_file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
+        # try:
+        #     with open(self.__file_path, mode='r') as retrieval_file:
+        #         if os.path.getsize(self.__file_path) > 0:
+        #             self.__objects = json.load(retrieval_file)
+        # except FileNotFoundError:
+        #     pass
+        from models.base_model import BaseModel
+
         try:
-            with open(self.__file_path, mode='r') as retrieval_file:
-                if os.path.getsize(self.__file_path) > 0:
-                    self.__objects = json.load(retrieval_file)
+            with open(FileStorage.__file_path, mode='r') as my_file:
+                new_dict = json.load(my_file)
+
+            for key, value in new_dict.items():
+                class_name = value.get('__class__')
+                obj = eval(class_name + '(**value)')
+                FileStorage.__objects[key] = obj
+
         except FileNotFoundError:
             pass
